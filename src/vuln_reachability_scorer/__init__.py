5cc0aff29c94ab0921c0d34952666ca39a3603d1
Ramkumar Ethiraj <64294490+rmkr-dev@users.noreply.github.com>
2026-09-14 08:54:19 +0000

    chore: bump version to 0.3.9

--- src/vuln_reachability_scorer/__init__.py
+++ src/vuln_reachability_scorer/__init__.py
@@ -1,6 +1,6 @@
 """Topology-aware vulnerability reachability scorer."""
 
-__version__ = "0.3.8"
+__version__ = "0.3.9"
 
 from vuln_reachability_scorer.models import Asset, Edge, Finding, ScoredFinding
 from vuln_reachability_scorer.scoring import score_findings
